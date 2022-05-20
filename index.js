d3d558a2bf6dbf917454d29669c5ef73e0f5d838
Juan <juanbeta00@hotmail.com>
2022-05-20 13:31:20 -0400

    commit tree 56379bd63e02c37981e8410fe13de052b6873e61

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 18;
+
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+ 
+const mod = num3 % num4;
+
+const max = Math.max(0, 5, 10, 15, 20);
+
